0f7035e24ef9faf67be9a63dfcbf76114f36be07
pictures2333 <m0987529191@gmail.com>
2023-11-01 14:03:44 +0800

    some zerojudge

---
+++ zerojudge/a147.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	while (cin>>n){
+		if (n == 0) break;
+		else{
+			for (int i=1; i<n;i++){
+				if ((i%7)!=0) cout << i << " ";
+			}
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
